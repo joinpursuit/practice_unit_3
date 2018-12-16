12e95c8144ab33bbf1b3478386ef6e1db10f415f
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2018-12-16 12:39:56 -0500

    I'm gonna start with just the user and post table for now

---
+++ db/seed.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS practice_unit_3;
+CREATE DATABASE practice_unit_3;
+
+\c practice_unit_3;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  age INT
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  poster_id INT REFERENCES users(id),
+  body VARCHAR NOT NULL
+);
+
+CREATE TABLE likes (
+  id SERIAL PRIMARY KEY,
+  liker_id INT REFERENCES users(id),
+  post_id INT REFERENCES posts(id)
+);
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  commenter_id INT REFERENCES users(id),
+  post_id INT REFERENCES posts(id),
+  body VARCHAR NOT NULL
+);
+
+CREATE TABLE albums (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id)
+);
+
+CREATE Table Pictures (
+  id SERIAL PRIMARY KEY,
+  album_id INT REFERENCES albums(id),
+  url VARCHAR NOT NULL
+);
+
+INSERT INTO users (name, age) VALUES ('Jonelle Bain', 100), ('Max Mezalon', 102);
+
+INSERT INTO posts (id, body) VALUES (2, 'this is my first posts'), (4, 'this is pretty cool');
