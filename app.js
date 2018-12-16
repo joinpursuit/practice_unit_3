41a754ad7d2dc3a9dc4ff003c6520d9d9c15571a
mateo-navarrete <mateonavarrete@pursuit.org>
2018-12-16 14:19:22 -0500

    added errRoutes & users routes & queries

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const users = require('./routes/users');
 
 const listenMsg = () => (
   console.log(`listening on port${port}`));
 
+const faviconMsg = (req, res, next) => (
+  req.originalUrl &&
+  req.originalUrl.split('/').pop() === 'favicon.ico' ?
+  res.sendStatus(204) : next());
+
+const catchAll = (req, res, next) => {
+  let err = new Error('Page Not Found');
+  err.statusCode = 404;
+  next(err);
+};
+
+const errMsg = (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err,
+  });
+  console.log(err.message);
+};
+
 const endRoute = (req, res) => (res.send('Home Page'));
 
+app.use(faviconMsg);
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+app.use(bodyParser.json());
+app.use('/users', users);
+
 app.get('/', endRoute);
+
+app.use('*', catchAll);
+app.use(errMsg);
 
 app.listen(port, listenMsg);
